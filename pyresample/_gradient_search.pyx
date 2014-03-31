ea4a3be689ea2864448aca86d56eb18b3488a5e5
Martin Raspaud <martin.raspaud@smhi.se>
2014-03-31 12:49:30 +0200

    Cleanup and bugfixes. Thanks Leon!
    Signed-off-by: Martin Raspaud <martin.raspaud@smhi.se>

--- pyresample/_gradient_search.pyx
+++ pyresample/_gradient_search.pyx
@@ -6,6 +6,7 @@
 # Author(s):
 
 #   Martin Raspaud <martin.raspaud@smhi.se>
+#   Leon Majewski  <leon.majewski@bom.gov.au>
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -29,108 +30,107 @@ cimport cython
 def fast_gradient_search(np.ndarray[DTYPE_t, ndim=2] data, np.ndarray[DTYPE_t, ndim=2] source_x, np.ndarray[DTYPE_t, ndim=2] source_y, area_extent, size):
     """Trishchenko stuff.
     """
+    #area extent bounds --> area_def.area_extent
     cdef double x_min, y_min, x_max, y_max
     x_min, y_min, x_max, y_max = area_extent
+    #change the output size (x_size, y_size) to match area_def.shape: (lines,pixels)
     cdef int x_size, y_size
-    x_size, y_size = size
-
+    y_size, x_size = size
+    #step in x direction (meters); x-output locations (centre of pixel); repeat for y
     cdef double x_inc = (x_max - x_min) / x_size
     cdef np.ndarray[DTYPE_t, ndim=1] x_1d = np.arange(x_min + x_inc/2, x_max, x_inc)
     cdef double y_inc = (y_max - y_min) / y_size
-
     cdef np.ndarray[DTYPE_t, ndim=1] y_1d = np.arange(y_min + y_inc/2, y_max, y_inc)
-
-    cdef np.ndarray[DTYPE_t, ndim=2] image = np.zeros([x_size, y_size], dtype=DTYPE)
-
+    #output image array --> needs to be (lines, pixels) --> y,x
+    cdef np.ndarray[DTYPE_t, ndim=2] image = np.zeros([y_size, x_size], dtype=DTYPE)
+    #gradient of output y/x grids (in pixel and line directions)
     cdef np.ndarray[DTYPE_t, ndim=2] yp, yl
     yp, yl = np.gradient(source_y)
     cdef np.ndarray[DTYPE_t, ndim=2] xp, xl
     xp, xl = np.gradient(source_x)
-
     xp = -xp
     xl = -xl
     yp = -yp
     yl = -yl
-
+    #pixel max ---> data is expected in [lines, pixels]
     cdef int pmax = data.shape[1] - 1
     cdef int lmax = data.shape[0] - 1
-
+    #centre of input image - starting point
     cdef int p0 = pmax / 2
     cdef int l0 = lmax / 2
-
-    cdef int l_a, l_b, p_a, p_b
-
-    #cols = range(len(y_1d))
-    #cols.reverse()
-    lines = range(x_size)
-
-    cdef int maxcol = 2048
-    cdef int maxline = 5680
-
+    #intermediate variables:
+    cdef int l_a, l_b, p_a, p_b, nnl, nnp
     cdef size_t i, j
     cdef double dx, dy, d, dl, dp, w_l, w_p
-
+    #number of iterations
     cdef int cnt = 0
-
-
-
+    #this was a bit confusing -- "lines" was based on x_size; change this variable to elements - make it a numpy array (long==numpy int dtype)
+    cdef np.ndarray[size_t, ndim=1] elements = np.arange(x_size, dtype=np.uintp)
     for i in range(y_size):
-        lines.reverse()
-        for j in lines:
+        #lines.reverse() --> swapped to elements - provide a reverse view of the array
+        elements = elements[::-1]
+        for j in elements:
             cnt = 0
             while True:
                 cnt += 1
                 # algorithm does not converge.
                 if cnt > 5:
                     break
-
+                #check we are within the input image bounds
                 if l0 < lmax and l0 >= 0 and p0 < pmax and p0 >= 0:
+                    #step size
                     dx = x_1d[j] - source_x[l0, p0]
                     dy = y_1d[i] - source_y[l0, p0]
                 else:
+                    #reset such that we are back in the input image bounds
                     l0 = max(0, min(lmax - 1, l0))
                     p0 = max(0, min(pmax - 1, p0))
                     break
-
+                #distance from pixel/line to output location
                 d = yl[l0, p0]*xp[l0, p0] - yp[l0, p0]*xl[l0, p0]
                 dl = -(yl[l0, p0]*dx - xl[l0, p0]*dy) / d
                 dp = (yp[l0, p0]*dx - xp[l0, p0]*dy) / d
-
+                #check that our distance to an output location is less than 1 pixel/line
                 if abs(dp) < 1 and abs(dl) < 1:
-                    if l0 < lmax and l0 >= 0 and p0 < pmax and p0 >= 0:
-                        # bilinear interpolation
-                        if dl < 0:
-                            l_a = l0 - 1
-                            l_b = l0
-                            w_l = 1 + dl
-                        else:
-                            l_a = l0
-                            l_b = l0 + 1
-                            w_l = dl
-
-                        if dp < 0:
-                            p_a = p0 - 1
-                            p_b = p0
-                            w_p = 1 + dp
-                        else:
-                            p_a = p0
-                            p_b = p0 + 1
-                            w_p = dp
-                        image[x_size - 1 - i, j] = ((1 - w_l) * (1 - w_p) * data[l_a, p_a] + 
-                                                    (1 - w_l) * w_p * data[l_a, p_b] + 
-                                                    w_l * (1 - w_p) * data[l_b, p_a] + 
-                                                    w_l * w_p * data[l_b, p_b])
-
-                        # simple mode
-                        #image[x_size - 1 - i, j] = data[l0, p0]
+                    # # nearest neighbour
+                    # nnl = l0
+                    # if dl < -0.5 and nnl > 0:
+                    #     nnl -= 1
+                    # elif dl > 0.5 and nnp < lmax - 1:
+                    #     nnl += 1
+                    # nnp = p0
+                    # if dp < -0.5 and nnp > 0:
+                    #     nnp -= 1
+                    # elif dp > 0.5 and nnp < pmax - 1:
+                    #     nnp += 1
+                    # image[y_size - 1 - i, j] = data[nnl, nnp]
+                    # bilinear interpolation
+                    if dl < 0:
+                        l_a = max(0, l0 - 1)
+                        l_b = l0
+                        w_l = 1 + dl
                     else:
-                        l0 = max(0, min(lmax - 1, l0))
-                        p0 = max(0, min(pmax - 1, p0))
+                        l_a = l0
+                        l_b = min(l0 + 1, lmax - 1)
+                        w_l = dl
+                    if dp < 0:
+                        p_a = max(0, p0 - 1)
+                        p_b = p0
+                        w_p = 1 + dp
+                    else:
+                        p_a = p0
+                        p_b = min(p0 + 1, pmax - 1)
+                        w_p = dp
+                    #assign image output... (was x_size in the first dimension --> needs to be y_size)
+                    image[y_size - 1 - i, j] = ((1 - w_l) * (1 - w_p) * data[l_a, p_a] + 
+                                                (1 - w_l) * w_p * data[l_a, p_b] + 
+                                                w_l * (1 - w_p) * data[l_b, p_a] + 
+                                                w_l * w_p * data[l_b, p_b])
+                    #found our solution, next
                     break
                 else:
+                    #increment...
                     l0 = int(l0 + dl)
                     p0 = int(p0 + dp)
-
-
-
+    #return the output image
     return image
--- pyresample/gradient_search.py
+++ pyresample/gradient_search.py
@@ -26,6 +26,8 @@
 from mpop.satellites import PolarFactory
 from datetime import datetime
 import mpop.scene
+import numpy as np
+import pyproj
 from mpop.utils import debug_on
 debug_on()
 
@@ -33,87 +35,42 @@ import mpop.utils
 mpop.utils.debug_on()
 import pyximport; pyximport.install()
 from _gradient_search import fast_gradient_search
+from pyresample import data_reduce
+
+def gradient_search(data, lons, lats, area):
+
+    tic = datetime.now()
+    p = pyproj.Proj(**area.proj_dict)
 
 
+    lon_bound, lat_bound = area.get_boundary_lonlats()
+    idx = data_reduce.get_valid_index_from_lonlat_boundaries(lon_bound,
+                                                             lat_bound,
+                                                             lons, lats,
+                                                             5000)
 
-def gradient_search(data, source_x, source_y, area_extent, size):
-    """Trishchenko stuff.
-    """
-    x_min, y_min, x_max, y_max = area_extent
-    x_size, y_size = size
-    gx = source_x
-    gy = source_y
+    colsmin, colsmax = np.arange(lons.shape[1])[np.sum(idx, 0, bool)][[0, -1]]
+    linesmin, linesmax = np.arange(lons.shape[0])[np.sum(idx, 1, bool)][[0, -1]]
+    projection_x_coords, projection_y_coords = p(lons[linesmin:linesmax,
+                                                      colsmin:colsmax],
+                                                 lats[linesmin:linesmax,
+                                                      colsmin:colsmax])
 
-    x_inc = (x_max - x_min) / x_size
-    x_1d = np.arange(x_min + x_inc/2, x_max, x_inc)
-    y_inc = (y_max - y_min) / y_size
+    toc2 = datetime.now()
+    print "pyproj took", toc2 - tic
+    tic2 = datetime.now()
+    image = fast_gradient_search(g[0.6].data[linesmin:linesmax,
+                                             colsmin:colsmax],
+                                 projection_x_coords, projection_y_coords,
+                                 area.area_extent,
+                                 area.shape)
 
-    y_1d = np.arange(y_min + y_inc/2, y_max, y_inc)
+    toc = datetime.now()
 
-    x, y = np.meshgrid(x_1d, y_1d)
-    image = np.zeros(x.shape)
-
-    yp, yl = np.gradient(gy)
-    xp, xl = np.gradient(gx)
-
-    xp = -xp
-    xl = -xl
-    yp = -yp
-    yl = -yl
-
-    p0, l0 = 1024, 2840
-
-    cols = range(len(y_1d))
-    cols.reverse()
-    lines = range(len(x_1d))
-
-    maxcol = 2048
-    maxline = 5680
-
-    prev_l0, prev_p0 = None, None
-
-    for i in cols:
-        lines.reverse()
-        for j in lines:
-            cnt = 0
-            while True:
-                cnt += 1
-                # algorithm does not converge.
-                if cnt > 5:
-                    image[x_size - 1 - i, j] = 0
-                    break
-
-                try:
-                    dx = x[i, j] - gx[l0, p0]
-                    dy = y[i, j] - gy[l0, p0]
-                except IndexError:
-                    image[x_size - 1 - i, j] = 0
-                    l0 = min(maxline - 1, l0)
-                    l0 = max(0, l0)
-                    p0 = min(maxcol - 1, p0)
-                    p0 = max(0, p0)
-                    break
-
-                d = yl[l0, p0]*xp[l0, p0] - yp[l0, p0]*xl[l0, p0]
-                dl = -(yl[l0, p0]*dx - xl[l0, p0]*dy) / d
-                dp = (yp[l0, p0]*dx - xp[l0, p0]*dy) / d
-
-                l0 += dl
-                p0 += dp
-
-                if abs(dp) < 1 and abs(dl) < 1:
-                    try:
-                        image[x_size - 1 - i, j] = data[l0, p0]
-
-                    except IndexError:
-                        l0 = min(maxline - 1, l0)
-                        l0 = max(0, l0)
-                        p0 = min(maxcol - 1, p0)
-                        p0 = max(0, p0)
-                    break
-
-
+    print "resampling took", toc - tic
+    print "from which fast_gradient_search took", toc - tic2
     return image
+
 
 def show(data):
     """Show the stetched data.
@@ -130,33 +87,18 @@ if __name__ == '__main__':
     g = PolarFactory.create_scene("noaa", "16", "avhrr", t, orbit="64374")
     g.load()
 
+    from mpop.projector import get_area_def
+
+    area = get_area_def("scan2")
+
     # for comparison
 
-    # tic = datetime.now()
-    # l = g.project("scan2")
-    # toc = datetime.now()
-    # print "pyresample took", toc - tic
-
-
-
-
-    import numpy as np
-    import pyproj
-
     tic = datetime.now()
-    p = pyproj.Proj(proj="stere", ellps="bessel", lat_0=90, lon_0=14, lat_ts=60)
-    g[0.6].area = g.area
-    gx, gy = p(g[0.6].area.lons, g[0.6].area.lats)
-
-    # map grid for area "scan2"
-    tic2 = datetime.now()
-    image = fast_gradient_search(g[0.6].data, gx, gy,
-                            (-1268854.1266382949, -4150234.8425892727,
-                             779145.8733617051, -2102234.8425892727),
-                            (1024, 1024))
-
+    l = g.project(area)
     toc = datetime.now()
+    print "pyresample took", toc - tic
 
-    print "resampling took", toc - tic
-    print "fast_gradient_search took", toc - tic2
-    show(image)
+
+    res = gradient_search(g[0.6].data, g.area.lons, g.area.lats, area)
+
+    show(res)
