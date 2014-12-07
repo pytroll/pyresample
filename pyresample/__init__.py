42a71a70861c4aa57d6b5e5c8bfda33d7cc9beb8
Martin Raspaud <martin.raspaud@smhi.se>
2014-12-07 23:15:59 +0100

    remove some relative imports.
    Signed-off-by: Martin Raspaud <martin.raspaud@smhi.se>

--- pyresample/__init__.py
+++ pyresample/__init__.py
@@ -1,44 +1,45 @@
-#pyresample, Resampling of remote sensing image data in python
-# 
-#Copyright (C) 2010  Esben S. Nielsen
+# pyresample, Resampling of remote sensing image data in python
 #
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
+# Copyright (C) 2010, 2014  Esben S. Nielsen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
 #(at your option) any later version.
 #
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import absolute_import
 
-from . import grid
-from . import image
-from . import kd_tree
-from . import utils
-from . import version
-from . import plot
+from pyresample import grid
+from pyresample import image
+from pyresample import kd_tree
+from pyresample import utils
+from pyresample import version
+from pyresample import plot
 
 __version__ = version.__version__
+
 
 def get_capabilities():
     cap = {}
 
     try:
         from pykdtree.kdtree import KDTree
-        cap['pykdtree'] = True 
+        cap['pykdtree'] = True
     except ImportError:
         cap['pykdtree'] = False
 
     try:
         import numexpr
-        cap['numexpr'] = True 
+        cap['numexpr'] = True
     except ImportError:
-        cap['numexpr'] = False 
+        cap['numexpr'] = False
 
     return cap
