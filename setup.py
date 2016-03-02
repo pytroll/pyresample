ce598c1928afad11fd4dbb28e2fc33716747a170
Martin Raspaud <martin.raspaud@smhi.se>
2016-03-02 11:44:10 +0100

    Adapt to newest pykdtree version
    The kdtree object's attribute `data_pts` has been renamed to `data`.
    
    Signed-off-by: Martin Raspaud <martin.raspaud@smhi.se>

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ import imp
 version = imp.load_source('pyresample.version', 'pyresample/version.py')
 
 requirements = ['pyproj', 'numpy', 'configobj']
-extras_require = {'pykdtree': ['pykdtree'],
+extras_require = {'pykdtree': ['pykdtree>=1.1.1'],
                   'numexpr': ['numexpr'],
                   'quicklook': ['matplotlib', 'basemap']}
 
