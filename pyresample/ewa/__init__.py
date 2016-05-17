Fix bug in EWA grid origin calculation
Forgot that cell height was negative so ended up subtracting a negative, going in the wrong direction for the Y origin of the grid.

pyresample/ewa/__init__.py
@@ -118,7 +118,7 @@ def ll2cr(swath_def, area_def, fill=np.nan, copy=True):
     w = area_def.x_size
     h = area_def.y_size
     ox = area_def.area_extent[0] + cw / 2.
-    oy = area_def.area_extent[3] - ch / 2.
+    oy = area_def.area_extent[3] + ch / 2.
     swath_points_in_grid = _ll2cr.ll2cr_static(lons, lats, fill,
                                                p, cw, ch, w, h, ox, oy)
     return swath_points_in_grid, lons, lats