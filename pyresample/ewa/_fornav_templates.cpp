3ca66db5658b1ae021efb876982f6e1f878d4b7b
davidh-ssec <david.hoese@ssec.wisc.edu>
2016-07-14 22:02:16 -0500

    Fix isnan definition only if a macro doesn't already exist
    Numpy does some special macro stuff to define a good npy_isnan function. Some systems define a macro for it, others don't. Hopefully this works for all systems. A better solution might be to define a templated isnan that calls npy_isnan if it isn't an integer.

--- pyresample/ewa/_fornav_templates.cpp
+++ pyresample/ewa/_fornav_templates.cpp
@@ -7,7 +7,9 @@
 
 // Overload isnan for integer values
 // Otherwise some newer Windows compilers will raise an error
-int inline isnan(npy_int8 x) { return 0;}
+#ifndef isnan
+    int inline isnan(npy_int8 x) { return 0;}
+#endif
 
 int initialize_weight(size_t chan_count, unsigned int weight_count, weight_type weight_min, weight_type weight_distance_max,
         weight_type weight_delta_max, weight_type weight_sum_min, ewa_weight *ewaw) {
