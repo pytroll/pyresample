6689cb8e90db23b5f9f7dbeeb0727955faa70f29
Panu Lahtinen <pnuu+git@iki.fi>
2017-01-26 16:00:04 +0200

    Initial version of bilinear resampling.
    NOTE: Only works if both source and destination are area definitions.
    Also to be added is handling for the cases where a__ equals zero (use
    linear solution of bx + c = 0), testing, logging and all the error
    handling.

---
+++ pyresample/bilinear/__init__.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2017
+
+# Author(s):
+
+#   Panu Lahtinen <panu.lahtinen@fmi.fi>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Code for resampling using bilinear algorithm for irregular grids.
+
+The algorithm is taken from
+
+http://www.ahinson.com/algorithms_general/Sections/InterpolationRegression/InterpolationIrregularBilinear.pdf
+
+"""
+
+import logging
+import numpy as np
+from pyproj import Proj
+
+from pyresample import kd_tree
+
+LOG = logging.getLogger(__name__)
+
+
+def resample_bilinear(data, t__, s__, input_idxs, new_idx_arr, output_shape,
+                      fill_value=0):
+    """Resample using irregular bilinear algorithm."""
+
+    # Ravel the data
+    new_data = data.ravel()[input_idxs]
+    data_min = np.nanmin(new_data)
+    data_max = np.nanmax(new_data)
+
+    new_data = new_data[new_idx_arr]
+
+    # print new_data.shape
+
+    # Get neighbour data to separate variables
+    p_1 = new_data[:, 0]
+    p_2 = new_data[:, 1]
+    p_3 = new_data[:, 2]
+    p_4 = new_data[:, 3]
+
+    result = (p_1 * (1 - s__) * (1 - t__) +
+              p_2 * s__ * (1 - t__) +
+              p_3 * (1 - s__) * t__ +
+              p_4 * s__ * t__)
+
+    mask = ((result > data_max) | (result < data_min) |
+            np.isnan(result) | result.mask)
+
+    result = np.ma.masked_where(mask, result.data)
+
+    result = result.reshape(output_shape)
+
+    return result
+
+
+def calc_params(in_area, out_area, radius, neighbours=16, nprocs=1):
+    """Calculate parameters *s* and *t* for bilinear parametric lines"""
+
+    # Calculate neighbour information
+    (input_idxs, output_idxs, idx_ref, dists) = \
+        kd_tree.get_neighbour_info(in_area, out_area,
+                                   radius, neighbours=neighbours,
+                                   nprocs=nprocs)
+
+    del output_idxs, dists
+
+    # Get output projection as pyproj object
+    proj = Proj(out_area.proj4_string)
+
+    # Read output coordinates
+    out_lons, out_lats = out_area.get_lonlats()
+    out_lons, out_lats = mask_coordinates(out_lons, out_lats)
+
+    # Get input coordinates
+    in_lons, in_lats = in_area.get_lonlats()
+
+    # Select valid locations
+    in_lons = in_lons.ravel()[input_idxs]
+    in_lats = in_lats.ravel()[input_idxs]
+
+    # Mask invalid values
+    in_lons, in_lats = mask_coordinates(in_lons, in_lats)
+
+    # Reduce index reference
+    input_size = input_idxs.sum()
+    index_mask = (idx_ref == input_size)
+    idx_ref = np.where(index_mask, 0, idx_ref)
+
+    # Expand input coordinates for each output location
+    in_lons = in_lons[idx_ref]
+    in_lats = in_lats[idx_ref]
+
+    # Convert coordinates to output projection x/y space
+    in_x, in_y = proj(in_lons, in_lats)
+    del in_lons, in_lats
+    out_x, out_y = proj(out_lons, out_lats)
+    del out_lons, out_lats
+
+    # Find four closest pixels around the target location
+
+    # Tile output coordinates to same shape as neighbour info
+    out_x_tile = np.tile(out_x, (neighbours, 1)).T
+    out_y_tile = np.tile(out_y, (neighbours, 1)).T
+
+    # Get differences in both directions
+    x_diff = out_x_tile - in_x
+    y_diff = out_y_tile - in_y
+
+    stride = np.arange(x_diff.shape[0])
+
+    # Upper left source pixel
+    valid = (x_diff > 0) & (y_diff < 0)
+    x_1, y_1, idx_1 = get_corner(stride, valid, in_x, in_y, idx_ref)
+
+    # Upper right source pixel
+    valid = (x_diff < 0) & (y_diff < 0)
+    x_2, y_2, idx_2 = get_corner(stride, valid, in_x, in_y, idx_ref)
+
+    # Lower left source pixel
+    valid = (x_diff > 0) & (y_diff > 0)
+    x_3, y_3, idx_3 = get_corner(stride, valid, in_x, in_y, idx_ref)
+
+    # Lower right source pixel
+    valid = (x_diff < 0) & (y_diff > 0)
+    x_4, y_4, idx_4 = get_corner(stride, valid, in_x, in_y, idx_ref)
+
+    # Combine sorted indices to idx_ref
+    idx_ref = np.vstack((idx_1, idx_2, idx_3, idx_4)).T
+
+    # Pairwise longitudal separations between reference points
+    x_21 = x_2 - x_1
+    x_31 = x_3 - x_1
+    x_42 = x_4 - x_2
+
+    # Pairwise latitudal separations between reference points
+    y_21 = y_2 - y_1
+    y_31 = y_3 - y_1
+    y_42 = y_4 - y_2
+
+    # Parameters for 2nd order polynomial
+    a__ = x_31 * y_42 - y_31 * x_42
+    b__ = out_y * (x_42 - x_31) - out_x * (y_42 - y_31) + \
+        x_31 * y_2 - y_31 * x_2 + x_1 * y_42 - y_1 * x_42
+    c__ = out_y * x_21 - out_x * y_21 + x_1 * y_2 - x_2 * y_1
+
+    # Mask out division by zero
+    a__ = np.ma.masked_where(a__ == 0, a__)
+
+    # Mask out invalid (complex) discriminants
+    discriminant = b__ * b__ - 4 * a__ * c__
+    idxs = discriminant < 0
+    discriminant = np.ma.masked_where(idxs, discriminant)
+
+    # Solve the quadratic polynomial
+    t_1 = (-b__ + np.sqrt(discriminant)) / (2 * a__)
+    t_2 = (-b__ - np.sqrt(discriminant)) / (2 * a__)
+
+    # Find valid solutions, ie. 0 <= t <= 1
+    t__ = t_1.copy()
+    idxs = (t_1 < 0) | (t_1 > 1)
+    t__[idxs] = t_2[idxs]
+
+    idxs = (t__ < 0) | (t__ > 1)
+    t__ = np.ma.masked_where(idxs, t__)
+
+    s__ = (out_y - y_1 - y_31 * t__) / (y_2 + y_42 * t__ - y_1 - y_31 * t__)
+
+    idxs = (s__ < 0) | (s__ > 1)
+    s__ = np.ma.masked_where(idxs, s__)
+
+    return t__, s__, input_idxs, idx_ref
+
+
+def mask_coordinates(lons, lats):
+    """Mask invalid coordinate values"""
+    lons = lons.ravel()
+    lats = lats.ravel()
+    idxs = ((lons < -180.) | (lons > 180.) |
+            (lats < -90.) | (lats > 90.))
+    lons = np.ma.masked_where(idxs, lons)
+    lats = np.ma.masked_where(idxs, lats)
+
+    return lons, lats
+
+
+def get_corner(stride, valid, in_x, in_y, idx_ref):
+    """Get closest set of coordinates from the *valid* locations"""
+    idxs = np.argmax(valid, axis=1)
+    invalid = np.invert(np.max(valid, axis=1))
+    x__ = np.ma.masked_where(invalid, in_x[stride, idxs])
+    y__ = np.ma.masked_where(invalid, in_y[stride, idxs])
+    idx = idx_ref[stride, idxs]
+
+    return x__, y__, idx
