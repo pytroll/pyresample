5aca1c5e588b0c9d79c58e4934d13dfa3bf77b48
Martin Raspaud <martin.raspaud@smhi.se>
2017-03-22 11:21:31 +0100

    Bump version: 1.3.0 → 1.3.1

--- pyresample/version.py
+++ pyresample/version.py
@@ -15,4 +15,4 @@
 # You should have received a copy of the GNU Lesser General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '1.3.0'
+__version__ = '1.3.1'
