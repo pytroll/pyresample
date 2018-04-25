030fa08b19d05cd1f07837a6d6c650cfb94e9f02
Panu Lahtinen <pnuu+git@iki.fi>
2018-04-25 10:28:01 +0300

    Add resampling options that gives the known result

--- pyresample/test/test_bilinear.py
+++ pyresample/test/test_bilinear.py
@@ -181,7 +181,9 @@ class Test(unittest.TestCase):
 
     def test_get_bil_info(self):
         t__, s__, input_idxs, idx_arr = bil.get_bil_info(self.swath_def,
-                                                         self.target_def)
+                                                         self.target_def,
+                                                         50e5, neighbours=32,
+                                                         nprocs=1)
 
         # Only 6th index should have valid values
         for i in range(len(t__)):
@@ -195,14 +197,20 @@ class Test(unittest.TestCase):
                 self.assertTrue(np.isnan(s__[i]))
             # All the others should have values between 0.0 and 1.0
             else:
-                self.assertTrue(t__[i] >= 0.0)
-                self.assertTrue(s__[i] >= 0.0)
-                self.assertTrue(t__[i] <= 1.0)
-                self.assertTrue(s__[i] <= 1.0)
+                try:
+                    self.assertTrue(t__[i] >= 0.0)
+                    self.assertTrue(s__[i] >= 0.0)
+                    self.assertTrue(t__[i] <= 1.0)
+                    self.assertTrue(s__[i] <= 1.0)
+                except AssertionError:
+                    print i, t__[i], s__[i]
+                    raise AssertionError
 
     def test_get_sample_from_bil_info(self):
         t__, s__, input_idxs, idx_arr = bil.get_bil_info(self.swath_def,
-                                                         self.target_def)
+                                                         self.target_def,
+                                                         50e5, neighbours=32,
+                                                         nprocs=1)
         # Sample from data1
         res = bil.get_sample_from_bil_info(self.data1.ravel(), t__, s__,
                                            input_idxs, idx_arr)
@@ -230,17 +238,22 @@ class Test(unittest.TestCase):
         # Single array
         res = bil.resample_bilinear(self.data1,
                                     self.swath_def,
-                                    self.target_def)
+                                    self.target_def,
+                                    50e5, neighbours=32,
+                                    nprocs=1)
         self.assertEqual(res.shape, self.target_def.shape)
-        # There are 12 pixels with value 1, all others are 0
+        # There are 12 pixels with value 1, all others are zero
         self.assertEqual(res.sum(), 12)
+        self.assertEqual((res == 0).sum(), 4)
 
         # Single array with masked output
         res = bil.resample_bilinear(self.data1,
                                     self.swath_def,
-                                    self.target_def, fill_value=None)
+                                    self.target_def,
+                                    50e5, neighbours=32,
+                                    nprocs=1, fill_value=None)
         self.assertTrue(hasattr(res, 'mask'))
-        # There should be 8 valid pixels
+        # There should be 12 valid pixels
         self.assertEqual(self.target_def.size - res.mask.sum(), 12)
 
         # Two stacked arrays
