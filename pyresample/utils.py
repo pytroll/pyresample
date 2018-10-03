6e87b4915fe83b50aa851c9b43bc963ca330f38a
Martin Raspaud <martin.raspaud@smhi.se>
2018-10-03 21:14:11 +0200

    Bugfix converter

--- pyresample/utils.py
+++ pyresample/utils.py
@@ -577,8 +577,7 @@ def convert_def_to_yaml(def_area_file, yaml_area_file):
 
     *yaml_area_file* will be overwritten by the operation.
     """
-    from pyresample.utils import parse_area_file
-    areas = parse_area_file('satpy/satpy/etc/areas.def')
+    areas = parse_area_file(def_area_file)
     with open(yaml_area_file, 'w') as yaml_file:
         for area in areas:
             yaml_file.write(area.create_areas_def())
