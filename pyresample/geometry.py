c847bed867615bc657249fd046116fd69bc44b51
Stephan Finkensieper <stephan.finkensieper@dwd.de>
2019-03-26 09:28:39 +0000

    Remove print statement

--- pyresample/geometry.py
+++ pyresample/geometry.py
@@ -884,7 +884,6 @@ class AreaDefinition(BaseDefinition):
         self.area_extent = tuple(area_extent)
 
         # Calculate area_extent in lon lat
-        print(proj_dict)
         proj = Proj(**proj_dict)
         corner_lons, corner_lats = proj((area_extent[0], area_extent[2]),
                                         (area_extent[1], area_extent[3]),
