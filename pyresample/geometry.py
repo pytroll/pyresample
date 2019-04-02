89595e5ce24ee16d5e271ef84eb7c9a786ff0d1d
Martin Raspaud <martin.raspaud@smhi.se>
2019-04-03 00:08:25 +0200

    Fix lons and lats resolution after aggregation

--- pyresample/geometry.py
+++ pyresample/geometry.py
@@ -575,6 +575,9 @@ class SwathDefinition(CoordinateDefinition):
                          coords=res.coords, attrs=self.lons.attrs.copy())
         lats = DataArray(lonlatalt[:, :, 1], dims=self.lons.dims,
                          coords=res.coords, attrs=self.lons.attrs.copy())
+        resolution = lons.attrs['resolution'] / (dims.get('x', 1) + dims.get('y', 1)) / 2
+        lons.attrs['resolution'] = resolution
+        lats.attrs['resolution'] = resolution
         return SwathDefinition(lons, lats)
 
     def __hash__(self):
