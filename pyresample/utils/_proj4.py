f7a8c6509b7c376e9b756895ec1d3c02e59fd59a
Stephan Finkensieper <stephan.finkensieper@dwd.de>
2019-04-15 15:11:00 +0000

    Fix unused import

--- pyresample/utils/_proj4.py
+++ pyresample/utils/_proj4.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU Lesser General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 from collections import OrderedDict
-import warnings
 
 
 def convert_proj_floats(proj_pairs):
