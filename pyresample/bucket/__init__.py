24aafb8daf3ee823f44791a99d312c0bfd60070e
Panu Lahtinen <pnuu+git@iki.fi>
2019-05-17 17:55:28 +0300

    Remove author lines

--- pyresample/bucket/__init__.py
+++ pyresample/bucket/__init__.py
@@ -2,10 +2,6 @@
 #
 # Copyright (C) 2019  Pytroll developers
 #
-# Author(s):
-#
-#   Panu Lahtinen <panu.lahtinen@fmi.fi>
-#
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option) any
