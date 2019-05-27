38808fdfc230610157450179874393e62ce22f13
Panu Lahtinen <pnuu+git@iki.fi>
2019-05-27 08:09:26 +0300

    Change copyright holder from "Pytroll developers" to "Pyresample developers"

--- pyresample/bucket/__init__.py
+++ pyresample/bucket/__init__.py
@@ -1,6 +1,6 @@
 # pyresample, Resampling of remote sensing image data in python
 #
-# Copyright (C) 2019  Pytroll developers
+# Copyright (C) 2019  Pyresample developers
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the Free
