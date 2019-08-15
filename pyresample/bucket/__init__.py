Fix bucket category calculation with newer versions of dask/numpy

pyresample/bucket/__init__.py
@@ -258,7 +258,7 @@ class BucketResampler(object):
         """
         if categories is None:
             LOG.warning("No categories given, need to compute the data.")
-            categories = np.unique(data)
+            categories = np.unique(data).compute()
         try:
             num = categories.size
         except AttributeError: