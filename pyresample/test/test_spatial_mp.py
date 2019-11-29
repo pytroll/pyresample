f378341f15457d4f6619192e184e14d8b88230b2
Nina Hakansson <nina.hakansson@smhi.se>
2019-11-29 13:58:54 +0100

    Added test that detects bug in transfrom_lonlats for integers

--- pyresample/test/test_spatial_mp.py
+++ pyresample/test/test_spatial_mp.py
@@ -16,18 +16,18 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""Testing the _spatial_mp module"""
+"""Testing the _spatial_mp module."""
 
 
-try:
-    from unittest import mock
-except ImportError:
-    # separate mock package py<3.3
-    import mock
-import pyproj
+# try:
+#     from unittest import mock
+# except ImportError:
+#     # separate mock package py<3.3
+#     import mock
 import unittest
+import numpy as np
 
-from pyresample._spatial_mp import BaseProj
+from pyresample._spatial_mp import Cartesian
 
 
 # class SpatialMPTest(unittest.TestCase):
@@ -48,13 +48,29 @@ from pyresample._spatial_mp import BaseProj
 #             proj_init.assert_called_with(projparams='EPSG:6932', preserve_units=mock.ANY)
 #             proj_init.reset_mock()
 
+class SpatialMPTest(unittest.TestCase):
+    """Test of spatial_mp."""
+
+    def test_cartesian(self):
+        """Test the transform_lonlats of class Cartesian."""
+        exp_coords = np.array([[6370997., 0, 0],
+                               [6178887.9339746, 1089504.6535337, 1106312.0189715],
+                               [5233097.4664751, 2440233.4244888, 2692499.6776952]])
+        lon = np.array([0, 10, 25])
+        lat = np.array([0, 10, 25])
+        my_cartesian = Cartesian()
+        coords_int = my_cartesian.transform_lonlats(lon, lat)
+        coords_float = my_cartesian.transform_lonlats(lon.astype(np.float64), lat.astype(np.float64))
+
+        np.testing.assert_almost_equal(coords_float, exp_coords, decimal=3)
+        np.testing.assert_almost_equal(coords_int, coords_float, decimal=0)
+
 
 def suite():
-    """The test suite.
-    """
-    # loader = unittest.TestLoader()
+    """Test suite for _spatial_mp."""
+    loader = unittest.TestLoader()
     mysuite = unittest.TestSuite()
-    # mysuite.addTest(loader.loadTestsFromTestCase(SpatialMPTest))
+    mysuite.addTest(loader.loadTestsFromTestCase(SpatialMPTest))
     return mysuite
 
 
