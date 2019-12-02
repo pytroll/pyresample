033cff18226bab780f9cfd7737238487d40958b0
Nina Hakansson <nina.hakansson@smhi.se>
2019-12-02 21:25:27 +0100

    Added test of types

--- pyresample/test/test_spatial_mp.py
+++ pyresample/test/test_spatial_mp.py
@@ -27,7 +27,7 @@
 import unittest
 import numpy as np
 
-from pyresample._spatial_mp import Cartesian
+import pyresample._spatial_mp as sp
 
 
 # class SpatialMPTest(unittest.TestCase):
@@ -51,21 +51,42 @@ from pyresample._spatial_mp import Cartesian
 class SpatialMPTest(unittest.TestCase):
     """Test of spatial_mp."""
 
+    def setUp(self):
+        """Set up some variables."""
+        self.exp_coords = np.array([[6370997., 0, 0],
+                                    [6178887.9339746, 1089504.6535337, 1106312.0189715],
+                                    [5233097.4664751, 2440233.4244888, 2692499.6776952]])
+        self.lon = np.array([0, 10, 25])
+        self.lat = np.array([0, 10, 25])
+        self.lon32 = self.lon.astype(np.float32)
+        self.lat32 = self.lon.astype(np.float32)
+        self.lon64 = self.lon.astype(np.float64)
+        self.lat64 = self.lon.astype(np.float64)
+
     def test_cartesian(self):
         """Test the transform_lonlats of class Cartesian."""
-        exp_coords = np.array([[6370997., 0, 0],
-                               [6178887.9339746, 1089504.6535337, 1106312.0189715],
-                               [5233097.4664751, 2440233.4244888, 2692499.6776952]])
-        lon = np.array([0, 10, 25])
-        lat = np.array([0, 10, 25])
-        my_cartesian = Cartesian()
-        coords_int = my_cartesian.transform_lonlats(lon, lat)
-        coords_float = my_cartesian.transform_lonlats(lon.astype(np.float64), lat.astype(np.float64))
-        coords_float32 = my_cartesian.transform_lonlats(lon.astype(np.float32), lat.astype(np.float32))
-        np.testing.assert_almost_equal(coords_float, exp_coords, decimal=3)
+        my_cartesian = sp.Cartesian()
+        coords_int = my_cartesian.transform_lonlats(self.lon, self.lat)
+        coords_float = my_cartesian.transform_lonlats(self.lon64, self.lat64)
+        coords_float32 = my_cartesian.transform_lonlats(self.lon32, self.lat32)
+        np.testing.assert_almost_equal(coords_float, self.exp_coords, decimal=3)
         np.testing.assert_almost_equal(coords_int, coords_float, decimal=0)
         self.assertIs(type(coords_float32[0, 0]), np.float32)
         self.assertIs(type(coords_float[0, 0]), np.float64)
+        self.assertTrue(np.issubdtype(coords_int.dtype, np.floating))
+
+    def test_cartesian_without_ne(self):
+        """Test the transform_lonlats of class Cartesian without numexpr."""
+        sp.ne = False
+        my_cartesian = sp.Cartesian()
+        coords_int = my_cartesian.transform_lonlats(self.lon, self.lat)
+        coords_float = my_cartesian.transform_lonlats(self.lon64, self.lat64)
+        coords_float32 = my_cartesian.transform_lonlats(self.lon32, self.lat32)
+        np.testing.assert_almost_equal(coords_float, self.exp_coords, decimal=3)
+        np.testing.assert_almost_equal(coords_int, coords_float, decimal=0)
+        self.assertIs(type(coords_float32[0, 0]), np.float32)
+        self.assertIs(type(coords_float[0, 0]), np.float64)
+        self.assertTrue(np.issubdtype(coords_int.dtype, np.floating))
 
 
 def suite():
