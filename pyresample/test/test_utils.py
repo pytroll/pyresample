556e8b999dd9c2c21307e01c73e4fed63e1afd2c
Thomas Lavergne <thomas.lavergne@met.no>
2020-05-05 13:24:13 +0200

    more tests (and test data)

--- pyresample/test/test_utils.py
+++ pyresample/test/test_utils.py
@@ -641,24 +641,47 @@ class TestNetcdfCFAreaParser(unittest.TestCase):
         # try to load using a variable= that is a valid grid_mapping container, but use wrong x= and y=
         self.assertRaises(ValueError, load_cf_area, cf_file, 'Lambert_Azimuthal_Grid', y='time', x='xc',)
 
-    def test_load_cf_from_filepath(self):
+    def test_load_cf_nh25km_from_filepath(self):
         from pyresample.utils import load_cf_area
 
-        def validate_adef(adef):
+        def validate_nh25km_adef(adef):
             self.assertEqual(adef.shape,(432,432))
-            xc, yc = adef.get_proj_vectors()
+            xc = adef.projection_x_coords
+            yc = adef.projection_y_coords
             self.assertEqual(xc[0],-5387500.0, msg="Wrong x axis (index 0)")
-            self.assertEqual(xc[1],-5387500.0 + 25000.0, msg="Wrong x axis (index 1)")
+            self.assertEqual(xc[1],xc[0] + 25000.0, msg="Wrong x axis (index 1)")
             self.assertEqual(yc[0],5387500.0, msg="Wrong y axis (index 0)")
-            self.assertEqual(yc[1],5387500.0 - 25000.0, msg="Wrong y axis (index 1)")
+            self.assertEqual(yc[1],yc[0] - 25000.0, msg="Wrong y axis (index 1)")
 
         cf_file = os.path.join(os.path.dirname(__file__), 'test_files', 'cf_nh25km.nc')
 
         # load using a variable= that has a :grid_mapping attribute
         adef_1 = load_cf_area(cf_file, 'ice_conc')
-        validate_adef(adef_1)
+        validate_nh25km_adef(adef_1)
 
         # load using a variable= that is a valid grid_mapping container
         adef_2 = load_cf_area(cf_file, 'Lambert_Azimuthal_Grid', y='yc', x='xc',)
-        validate_adef(adef_2)
+        validate_nh25km_adef(adef_2)
+
+    def test_load_cf_nh10km_from_filepath(self):
+        from pyresample.utils import load_cf_area
+
+        def validate_nh10km_adef(adef):
+            self.assertEqual(adef.shape,(1120, 760))
+            xc = adef.projection_x_coords
+            yc = adef.projection_y_coords
+            self.assertEqual(xc[0],-3845000.0, msg="Wrong x axis (index 0)")
+            self.assertEqual(xc[1],xc[0] + 10000.0, msg="Wrong x axis (index 1)")
+            self.assertEqual(yc[0],5845000.0, msg="Wrong y axis (index 0)")
+            self.assertEqual(yc[1],yc[0] - 10000.0, msg="Wrong y axis (index 1)")
+
+        cf_file = os.path.join(os.path.dirname(__file__), 'test_files', 'cf_nh10km.nc')
+
+        # load using a variable= that has a :grid_mapping attribute
+        adef_1 = load_cf_area(cf_file, 'ice_conc')
+        validate_nh10km_adef(adef_1)
+
+        # load using a variable= that is a valid grid_mapping container
+        adef_2 = load_cf_area(cf_file, 'Polar_Stereographic_Grid', y='yc', x='xc',)
+        validate_nh10km_adef(adef_2)
 
