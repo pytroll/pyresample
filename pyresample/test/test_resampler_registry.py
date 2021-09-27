0c243b19f73e6a01260d40b182a8b3887184f82d
David Hoese <david.hoese@ssec.wisc.edu>
2021-09-27 14:50:23 -0500

    Refactor resampler registry tests

--- pyresample/test/test_resampler_registry.py
+++ pyresample/test/test_resampler_registry.py
@@ -37,37 +37,34 @@ class TestResamplerRegistryManipulation:
         self.mock_reg.stop()
 
     def test_manual_resampler_registration(self):
-        from pyresample.future import register_resampler, unregister_resampler, list_resamplers
+        from pyresample.future import unregister_resampler, list_resamplers
         from pyresample.future import Resampler
         rname = "my_resampler"
-        assert rname not in list_resamplers()
-        register_resampler(rname, Resampler)
-        assert rname in list_resamplers()
+        _register_resampler_class(rname, Resampler)
         unregister_resampler(rname)
         assert rname not in list_resamplers()
 
-    def test_multiple_registration_warning(self):
+    def test_multiple_registration_warning_same_class(self):
         import warnings
-        from pyresample.future import register_resampler, list_resamplers
         from pyresample.future import Resampler
         rname = "my_resampler"
-        assert rname not in list_resamplers()
-        register_resampler(rname, Resampler)
-        assert rname in list_resamplers()
+        _register_resampler_class(rname, Resampler)
 
         with warnings.catch_warnings(record=True) as w:
             # same class
-            register_resampler(rname, Resampler)
-        assert len(w) >= 1
-        msgs = [msg.message.args[0].lower() for msg in w]
-        assert any("already registered" in msg for msg in msgs)
+            _register_resampler_class(rname, Resampler, no_exist=False)
+        _warn_message_in_warnings(w, "already registered")
+
+    def test_multiple_registration_warning_diff_class(self):
+        import warnings
+        from pyresample.future import Resampler
+        rname = "my_resampler"
+        _register_resampler_class(rname, Resampler)
 
         with warnings.catch_warnings(record=True) as w:
             # different class
-            register_resampler(rname, _custom_resampler_class())
-        assert len(w) >= 1
-        msgs = [msg.message.args[0].lower() for msg in w]
-        assert any("replacing" in msg for msg in msgs)
+            _register_resampler_class(rname, _custom_resampler_class(), no_exist=False)
+        _warn_message_in_warnings(w, "replacing")
 
     @pytest.mark.parametrize(
         "names",
@@ -107,6 +104,20 @@ def _custom_resampler_class():
             """Pretend to be a compute method."""
             return None
     return _MyResampler
+
+
+def _warn_message_in_warnings(warnings: list, message: str):
+    assert len(warnings) >= 1
+    msgs = [msg.message.args[0].lower() for msg in warnings]
+    assert any(message in msg for msg in msgs)
+
+
+def _register_resampler_class(rname, rcls, no_exist=True):
+    from pyresample.future import register_resampler, list_resamplers
+    if no_exist:
+        assert rname not in list_resamplers()
+    register_resampler(rname, rcls)
+    assert rname in list_resamplers()
 
 
 class TestBuiltinResamplerRegistry:
