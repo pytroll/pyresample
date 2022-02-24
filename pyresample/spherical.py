Fix unwrap radians

pyresample/spherical.py
@@ -29,15 +29,14 @@ logger = logging.getLogger(__name__)
 EPSILON = 0.0000001
 
 
-def unwrap_radians(val, mod=np.pi):
+def _unwrap_radians(val, mod=np.pi):
     """Put *val* between -*mod* and *mod*."""
-    val = np.asarray(val)
     return (val + mod) % (2 * mod) - mod
 
 
 def modpi(val, mod=np.pi):
     """Put *val* between -*mod* and *mod*."""
-    return unwrap_radians(val, mod=mod)
+    return _unwrap_radians(val, mod=mod)
 
 
 class SCoordinate(object):
@@ -48,7 +47,7 @@ class SCoordinate(object):
     """
 
     def __init__(self, lon, lat):
-        self.lon = float(unwrap_radians(lon))
+        self.lon = float(_unwrap_radians(lon))
         self.lat = lat
 
     def cross2cart(self, point):
@@ -367,7 +366,7 @@ class SphPolygon:
             radius (optional, number): Radius of spherical planet.
         """
         self.vertices = vertices.astype(np.float64, copy=False)
-        self.lon = unwrap_radians(self.vertices[:, 0])
+        self.lon = _unwrap_radians(self.vertices[:, 0])
         self.lat = self.vertices[:, 1]
         self.radius = radius
         self.cvertices = np.array([np.cos(self.lat) * np.cos(self.lon),

pyresample/test/test_spherical.py
@@ -110,11 +110,13 @@ class TestRadiansFunctions(unittest.TestCase):
     """Test utils functions processing radians arrays."""
 
     def test_radian_unwrapping(self):
-        from pyresample.spherical import modpi, unwrap_radians
+        from pyresample.spherical import _unwrap_radians, modpi
 
         # Test points at np.pi are converted to -np.pi
-        assert np.allclose(modpi((-np.pi, np.pi)), (-np.pi, -np.pi))
-        assert np.allclose(unwrap_radians((-np.pi, np.pi)), (-np.pi, -np.pi))
+        assert np.allclose(modpi(-np.pi), -np.pi)
+        assert np.allclose(modpi(np.pi), -np.pi)
+        assert np.allclose(_unwrap_radians(-np.pi), -np.pi)
+        assert np.allclose(_unwrap_radians(np.pi), -np.pi)
 
 
 class TestSCoordinate(unittest.TestCase):