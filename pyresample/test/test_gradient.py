1b03e05346267cc6748728c3aec935c7738644af
Martin Raspaud <martin.raspaud@smhi.se>
2022-06-02 12:29:11 +0200

    Remove unneeded test class

--- pyresample/test/test_gradient.py
+++ pyresample/test/test_gradient.py
@@ -486,48 +486,6 @@ class TestRBGradientSearchResamplerArea2Area:
         assert res.shape == dst_area.shape
 
 
-class TestRBGradientSearchResamplerSwath2Area:
-    """Test RBGradientSearchResampler for the Swath to Area case."""
-
-    def setup(self):
-        """Set up the test case."""
-        from pyresample.gradient import ResampleBlocksGradientSearchResampler
-
-        chunks = 20
-
-        self.dst_area = AreaDefinition('euro40', 'euro40', None,
-                                       {'proj': 'stere', 'lon_0': 14.0,
-                                        'lat_0': 90.0, 'lat_ts': 60.0,
-                                        'ellps': 'bessel'},
-                                       102, 102,
-                                       (-2717181.7304994687, -5571048.14031214,
-                                        1378818.2695005313, -1475048.1403121399))
-
-        self.src_area = AreaDefinition(
-            'omerc_otf',
-            'On-the-fly omerc area',
-            None,
-            {'alpha': '8.99811271718795',
-             'ellps': 'sphere',
-             'gamma': '0',
-             'k': '1',
-             'lat_0': '0',
-             'lonc': '13.8096029486222',
-             'proj': 'omerc',
-             'units': 'm'},
-            50, 100,
-            (-1461111.3603, 3440088.0459, 1534864.0322, 9598335.0457)
-        )
-
-        self.lons, self.lats = self.src_area.get_lonlats(chunks=chunks)
-        xrlons = xr.DataArray(self.lons.persist())
-        xrlats = xr.DataArray(self.lats.persist())
-        self.src_swath = SwathDefinition(xrlons, xrlats)
-
-        self.resampler = ResampleBlocksGradientSearchResampler(self.src_swath,
-                                                               self.dst_area)
-
-
 class TestRBGradientSearchResamplerArea2Swath:
     """Test RBGradientSearchResampler for the Swath to Area case."""
 
