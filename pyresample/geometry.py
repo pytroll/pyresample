f2d5e8b542756c642e928ef0580edd517cf2ba21
David Hoese <david.hoese@ssec.wisc.edu>
2022-06-26 14:40:52 -0500

    Add future annotations import

--- pyresample/geometry.py
+++ pyresample/geometry.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """Classes for geometry operations."""
+from __future__ import annotations
 
 import hashlib
 import math
