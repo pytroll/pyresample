2869938d1c2e080cd522852fdb04e7eaabeb86e7
David Hoese <david.hoese@ssec.wisc.edu>
2022-11-07 08:48:03 -0600

    Update copyright year in pyresample/test/test_geometry/test_area.py
    Co-authored-by: Martin Raspaud <martin.raspaud@smhi.se>

--- pyresample/test/test_geometry/test_area.py
+++ pyresample/test/test_geometry/test_area.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2010-2020 Pyresample developers
+# Copyright (C) 2010-2022 Pyresample developers
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the Free
