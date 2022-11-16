fbe8c053308744d332255d2bd6c39693884f54c2
Gionata Ghiggi <gionata.ghiggi@gmail.com>
2022-11-16 07:58:45 +0100

    Update copyright
    Co-authored-by: David Hoese <david.hoese@ssec.wisc.edu>

--- pyresample/future/spherical/point.py
+++ pyresample/future/spherical/point.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2013 - 2021 Pyresample developers
+# Copyright (c) 2022 Pyresample developers
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
