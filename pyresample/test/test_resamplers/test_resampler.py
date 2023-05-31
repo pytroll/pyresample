9e6101a52c4230e330a2708ebe2487780c1e59a7
David Hoese <david.hoese@ssec.wisc.edu>
2023-05-31 11:01:57 -0500

    Fix performance regression in base resampler class when comparing geometries

--- pyresample/test/test_resamplers/test_resampler.py
+++ pyresample/test/test_resamplers/test_resampler.py
@@ -20,12 +20,14 @@ from __future__ import annotations
 
 from unittest import mock
 
+import dask.array as da
 import numpy as np
 import pytest
+import xarray as xr
 from pytest_lazyfixture import lazy_fixture
 
 from pyresample.future.resamplers.resampler import Resampler
-from pyresample.geometry import AreaDefinition
+from pyresample.geometry import AreaDefinition, SwathDefinition
 from pyresample.resampler import BaseResampler
 
 
@@ -76,13 +78,68 @@ def test_resampler(src, dst):
     assert resample_results.shape == dst.shape
 
 
-def test_base_resampler_does_nothing_when_src_and_dst_areas_are_equal():
+@pytest.mark.parametrize(
+    ("use_swaths", "copy_dst_swath"),
+    [
+        (False, None),
+        (True, None),  # same objects are equal
+        (True, "dask"),  # same dask tasks are equal
+        (True, "swath_def"),  # same underlying arrays are equal
+    ])
+def test_base_resampler_does_nothing_when_src_and_dst_areas_are_equal(_geos_area, use_swaths, copy_dst_swath):
     """Test that the BaseResampler does nothing when the source and target areas are the same."""
+    src_geom = _geos_area if not use_swaths else _xarray_swath_def_from_area(_geos_area)
+    dst_geom = src_geom
+    if copy_dst_swath == "dask":
+        dst_geom = _xarray_swath_def_from_area(_geos_area)
+    elif copy_dst_swath == "swath_def":
+        dst_geom = SwathDefinition(dst_geom.lons, dst_geom.lats)
+
+    resampler = BaseResampler(src_geom, dst_geom)
+    some_data = xr.DataArray(da.zeros(src_geom.shape, dtype=np.float64), dims=('y', 'x'))
+    assert resampler.resample(some_data) is some_data
+
+
+@pytest.mark.parametrize(
+    ("src_area", "numpy_swath"),
+    [
+        (False, False),
+        (False, True),
+        (True, False),
+    ])
+@pytest.mark.parametrize("dst_area", [False, True])
+def test_base_resampler_unequal_geometries(_geos_area, _geos_area2, src_area, numpy_swath, dst_area):
+    """Test cases where BaseResampler geometries are not considered equal."""
+    src_geom = _geos_area if src_area else _xarray_swath_def_from_area(_geos_area, numpy_swath)
+    dst_geom = _geos_area2 if dst_area else _xarray_swath_def_from_area(_geos_area2)
+    resampler = BaseResampler(src_geom, dst_geom)
+    some_data = xr.DataArray(da.zeros(src_geom.shape, dtype=np.float64), dims=('y', 'x'))
+    with pytest.raises(NotImplementedError):
+        resampler.resample(some_data)
+
+
+def _xarray_swath_def_from_area(area_def, use_numpy=False):
+    chunks = None if use_numpy else -1
+    lons_da, lats_da = area_def.get_lonlats(chunks=chunks)
+    lons = xr.DataArray(lons_da, dims=('y', 'x'))
+    lats = xr.DataArray(lats_da, dims=('y', 'x'))
+    swath_def = SwathDefinition(lons, lats)
+    return swath_def
+
+
+@pytest.fixture
+def _geos_area():
     src_area = AreaDefinition('src', 'src area', None,
                               {'ellps': 'WGS84', 'h': '35785831', 'proj': 'geos'},
                               100, 100,
                               (5550000.0, 5550000.0, -5550000.0, -5550000.0))
+    return src_area
 
-    resampler = BaseResampler(src_area, src_area)
-    some_data = np.zeros(src_area.shape, dtype=np.float64)
-    assert resampler.resample(some_data) is some_data
+
+@pytest.fixture
+def _geos_area2():
+    src_area = AreaDefinition('src', 'src area', None,
+                              {'ellps': 'WGS84', 'h': '35785831', 'proj': 'geos'},
+                              200, 200,
+                              (5550000.0, 5550000.0, -5550000.0, -5550000.0))
+    return src_area
