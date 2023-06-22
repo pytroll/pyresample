67f9df2862a2527a8142a28f65ff436f65127d38
Benjamin Rösner <benjamin.roesner@dwd.de>
2023-06-22 13:06:50 +0200

    fix: flake8

--- pyresample/test/test_formatting.py
+++ pyresample/test/test_formatting.py
@@ -17,15 +17,14 @@
 """Test html formatting."""
 
 import pyresample
-# from pyresample.geometry import AreaDefinition, SwathDefinition
-from .test_geometry.test_area import stere_area
-from .test_geometry.test_swath import _gen_swath_def_numpy, _gen_swath_def_xarray_dask
-
 from pyresample._formatting_html import area_repr, swath_area_attrs_section
 
-def test_area_def_cartopy_missing(monkeypatch, stere_area):
-    """Test missing cartopy installation."""
+from .test_geometry.test_area import stere_area  # noqa F401
+from .test_geometry.test_swath import _gen_swath_def_numpy, _gen_swath_def_xarray_dask
 
+
+def test_area_def_cartopy_missing(monkeypatch, stere_area):  # noqa F811
+    """Test missing cartopy installation."""
     with monkeypatch.context() as m:
         m.setattr(pyresample._formatting_html, "cart", False)
 
@@ -33,23 +32,21 @@ def test_area_def_cartopy_missing(monkeypatch, stere_area):
         assert "Note: If cartopy is installed a display of the area can be seen here" in area._repr_html_()
 
 
-def test_area_def_cartopy_installed(stere_area):
+def test_area_def_cartopy_installed(stere_area):  # noqa F811
     """Test cartopy installed."""
-    area = stere_area #geos_src_area
+    area = stere_area
     assert "Note: If cartopy is installed a display of the area can be seen here" not in area._repr_html_()
 
 
-def test_area_repr_w_static_files(stere_area):
+def test_area_repr_w_static_files(stere_area):  # noqa F811
     """Test area representation with static files (css/icons) included."""
-
     area_def = stere_area
     res = area_repr(area_def)
     assert "<style>" in res
 
 
-def test_area_repr_wo_static_files(stere_area):
+def test_area_repr_wo_static_files(stere_area):  # noqa F811
     """Test area representation without static files (css/icons) included."""
-
     area_def = stere_area
     res = area_repr(area_def, include_static_files=False)
     assert "<style>" not in res
@@ -57,7 +54,6 @@ def test_area_repr_wo_static_files(stere_area):
 
 def test_swath_area_attrs_section_w_numpy(create_test_swath):
     """Test SwathDefinition attrs section with numpy lons/lats."""
-
     swath_def = _gen_swath_def_numpy(create_test_swath)
     res = swath_area_attrs_section(swath_def)
     assert "class=\'xr-text-repr-fallback\'" not in res
@@ -65,7 +61,6 @@ def test_swath_area_attrs_section_w_numpy(create_test_swath):
 
 def test_swath_area_attrs_section_w_xarray(create_test_swath):
     """Test SwathDefinition attrs section with numpy lons/lats."""
-
     swath_def = _gen_swath_def_xarray_dask(create_test_swath)
     res = swath_area_attrs_section(swath_def)
     assert "class=\'xr-text-repr-fallback\'" in res
