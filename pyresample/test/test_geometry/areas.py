ea2942ec4869d5a82edd72e2ca2e507ea62b8891
ghiggi <gionata.ghiggi@gmail.com>
2023-12-10 21:57:38 +0100

    Rerun pre-commit

--- pyresample/test/test_geometry/areas.py
+++ pyresample/test/test_geometry/areas.py
@@ -16,7 +16,8 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """Define test AreaDefinitions."""
-import pytest 
+import pytest
+
 
 @pytest.fixture
 def stere_area(create_test_area):
