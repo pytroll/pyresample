815668aa9e9700b0d7721d39c2844e416b264849
David Hoese <david.hoese@ssec.wisc.edu>
2025-03-20 15:41:07 -0500

    Move proj4 utility tests to separate module

---
+++ pyresample/test/test_utils/test_proj4.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2025 Pyresample developers
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""Tests for pyproj/PROJ helpers."""
+import numpy as np
+
+
+def test_proj4_radius_parameters_provided():
+    """Test proj4_radius_parameters with a/b."""
+    from pyresample import utils
+    a, b = utils.proj4.proj4_radius_parameters(
+        '+proj=stere +a=6378273 +b=6356889.44891',
+    )
+    np.testing.assert_almost_equal(a, 6378273)
+    np.testing.assert_almost_equal(b, 6356889.44891)
+
+
+def test_proj4_radius_parameters_ellps():
+    """Test proj4_radius_parameters with ellps."""
+    from pyresample import utils
+    a, b = utils.proj4.proj4_radius_parameters(
+        '+proj=stere +ellps=WGS84',
+    )
+    np.testing.assert_almost_equal(a, 6378137.)
+    np.testing.assert_almost_equal(b, 6356752.314245, decimal=6)
+
+
+def test_proj4_radius_parameters_default():
+    """Test proj4_radius_parameters with default parameters."""
+    from pyresample import utils
+    a, b = utils.proj4.proj4_radius_parameters(
+        '+proj=lcc +lat_0=10 +lat_1=10',
+    )
+    # WGS84
+    np.testing.assert_almost_equal(a, 6378137.)
+    np.testing.assert_almost_equal(b, 6356752.314245, decimal=6)
+
+
+def test_proj4_radius_parameters_spherical():
+    """Test proj4_radius_parameters in case of a spherical earth."""
+    from pyresample import utils
+    a, b = utils.proj4.proj4_radius_parameters(
+        '+proj=stere +R=6378273',
+    )
+    np.testing.assert_almost_equal(a, 6378273.)
+    np.testing.assert_almost_equal(b, 6378273.)
+
+
+def test_proj4_str_dict_conversion():
+    from pyresample import utils
+
+    proj_str = "+proj=lcc +ellps=WGS84 +lon_0=-95 +lat_1=25.5 +no_defs"
+    proj_dict = utils.proj4.proj4_str_to_dict(proj_str)
+    proj_str2 = utils.proj4.proj4_dict_to_str(proj_dict)
+    proj_dict2 = utils.proj4.proj4_str_to_dict(proj_str2)
+    assert proj_dict == proj_dict2
+    assert isinstance(proj_dict['lon_0'], (float, int))
+    assert isinstance(proj_dict2['lon_0'], (float, int))
+    assert isinstance(proj_dict['lat_1'], float)
+    assert isinstance(proj_dict2['lat_1'], float)
+
+    # EPSG
+    proj_str = '+init=EPSG:4326'
+    proj_dict = utils.proj4.proj4_str_to_dict(proj_str)
+    proj_str2 = utils.proj4.proj4_dict_to_str(proj_dict)
+    proj_dict2 = utils.proj4.proj4_str_to_dict(proj_str2)
+    # pyproj usually expands EPSG definitions so we can only round trip
+    assert proj_dict == proj_dict2
+
+    proj_str = 'EPSG:4326'
+    proj_dict_exp2 = {'proj': 'longlat', 'datum': 'WGS84', 'no_defs': None, 'type': 'crs'}
+    proj_dict = utils.proj4.proj4_str_to_dict(proj_str)
+    assert proj_dict == proj_dict_exp2
+    # input != output for this style of EPSG code
+    # EPSG to PROJ.4 can be lossy
+    # self.assertEqual(utils._proj4.proj4_dict_to_str(proj_dict), proj_str)  # round-trip
+
+
+def test_proj4_str_dict_conversion_with_valueless_parameter():
+    from pyresample import utils
+
+    # Value-less south parameter
+    proj_str = "+ellps=WGS84 +no_defs +proj=utm +south +type=crs +units=m +zone=54"
+    proj_dict = utils.proj4.proj4_str_to_dict(proj_str)
+    proj_str2 = utils.proj4.proj4_dict_to_str(proj_dict)
+    proj_dict2 = utils.proj4.proj4_str_to_dict(proj_str2)
+    assert proj_dict == proj_dict2
+
+
+def test_convert_proj_floats():
+    from collections import OrderedDict
+
+    import pyresample.utils as utils
+
+    pairs = [('proj', 'lcc'), ('ellps', 'WGS84'), ('lon_0', '-95'), ('no_defs', True)]
+    expected = OrderedDict([('proj', 'lcc'), ('ellps', 'WGS84'), ('lon_0', -95.0), ('no_defs', True)])
+    assert utils.proj4.convert_proj_floats(pairs) == expected
+
+    # EPSG
+    pairs = [('init', 'EPSG:4326'), ('EPSG', 4326)]
+    for pair in pairs:
+        expected = OrderedDict([pair])
+        assert utils.proj4.convert_proj_floats([pair]) == expected
--- pyresample/test/test_utils/test_utils.py
+++ pyresample/test/test_utils/test_utils.py
@@ -83,146 +83,54 @@ class TestPreprocessing(unittest.TestCase):
         rows, cols = utils.generate_nearest_neighbour_linesample_arrays(grid, grid_dst, 12000.)
 
 
-class TestMisc:
-    """Test miscellaneous utilities."""
+def test_wrap_longitudes():
+    # test that we indeed wrap to [-180:+180[
+    from pyresample import utils
+    step = 60
+    lons = np.arange(-360, 360 + step, step)
+    assert (lons.min() < -180) and (lons.max() >= 180) and (+180 in lons)
+    wlons = utils.wrap_longitudes(lons)
+    assert not ((wlons.min() < -180) or (wlons.max() >= 180) or (+180 in wlons))
 
-    def test_wrap_longitudes(self):
-        # test that we indeed wrap to [-180:+180[
-        from pyresample import utils
-        step = 60
-        lons = np.arange(-360, 360 + step, step)
-        assert (lons.min() < -180) and (lons.max() >= 180) and (+180 in lons)
-        wlons = utils.wrap_longitudes(lons)
-        assert not ((wlons.min() < -180) or (wlons.max() >= 180) or (+180 in wlons))
 
-    def test_wrap_and_check(self):
-        from pyresample import utils
+def test_wrap_and_check():
+    from pyresample import utils
 
-        lons1 = np.arange(-135., +135, 50.)
-        lats = np.ones_like(lons1) * 70.
-        new_lons, new_lats = utils.check_and_wrap(lons1, lats)
-        assert lats is new_lats
-        np.testing.assert_allclose(lons1, new_lons)
+    lons1 = np.arange(-135., +135, 50.)
+    lats = np.ones_like(lons1) * 70.
+    new_lons, new_lats = utils.check_and_wrap(lons1, lats)
+    assert lats is new_lats
+    np.testing.assert_allclose(lons1, new_lons)
 
-        lons2 = np.where(lons1 < 0, lons1 + 360, lons1)
-        new_lons, new_lats = utils.check_and_wrap(lons2, lats)
-        assert lats is new_lats
-        # after wrapping lons2 should look like lons1
-        np.testing.assert_allclose(lons1, new_lons)
+    lons2 = np.where(lons1 < 0, lons1 + 360, lons1)
+    new_lons, new_lats = utils.check_and_wrap(lons2, lats)
+    assert lats is new_lats
+    # after wrapping lons2 should look like lons1
+    np.testing.assert_allclose(lons1, new_lons)
 
-        lats2 = lats + 25.
-        with pytest.raises(ValueError):
-            utils.check_and_wrap(lons1, lats2)
+    lats2 = lats + 25.
+    with pytest.raises(ValueError):
+        utils.check_and_wrap(lons1, lats2)
 
-    def test_proj4_radius_parameters_provided(self):
-        """Test proj4_radius_parameters with a/b."""
-        from pyresample import utils
-        a, b = utils.proj4.proj4_radius_parameters(
-            '+proj=stere +a=6378273 +b=6356889.44891',
-        )
-        np.testing.assert_almost_equal(a, 6378273)
-        np.testing.assert_almost_equal(b, 6356889.44891)
 
-    def test_proj4_radius_parameters_ellps(self):
-        """Test proj4_radius_parameters with ellps."""
-        from pyresample import utils
-        a, b = utils.proj4.proj4_radius_parameters(
-            '+proj=stere +ellps=WGS84',
-        )
-        np.testing.assert_almost_equal(a, 6378137.)
-        np.testing.assert_almost_equal(b, 6356752.314245, decimal=6)
+@pytest.mark.skipif(pyproj.__proj_version__ == "9.3.0", reason="Bug in PROJ causes inequality in EPSG comparison")
+def test_def2yaml_converter():
+    import tempfile
 
-    def test_proj4_radius_parameters_default(self):
-        """Test proj4_radius_parameters with default parameters."""
-        from pyresample import utils
-        a, b = utils.proj4.proj4_radius_parameters(
-            '+proj=lcc +lat_0=10 +lat_1=10',
-        )
-        # WGS84
-        np.testing.assert_almost_equal(a, 6378137.)
-        np.testing.assert_almost_equal(b, 6356752.314245, decimal=6)
-
-    def test_proj4_radius_parameters_spherical(self):
-        """Test proj4_radius_parameters in case of a spherical earth."""
-        from pyresample import utils
-        a, b = utils.proj4.proj4_radius_parameters(
-            '+proj=stere +R=6378273',
-        )
-        np.testing.assert_almost_equal(a, 6378273.)
-        np.testing.assert_almost_equal(b, 6378273.)
-
-    def test_convert_proj_floats(self):
-        from collections import OrderedDict
-
-        import pyresample.utils as utils
-
-        pairs = [('proj', 'lcc'), ('ellps', 'WGS84'), ('lon_0', '-95'), ('no_defs', True)]
-        expected = OrderedDict([('proj', 'lcc'), ('ellps', 'WGS84'), ('lon_0', -95.0), ('no_defs', True)])
-        assert utils.proj4.convert_proj_floats(pairs) == expected
-
-        # EPSG
-        pairs = [('init', 'EPSG:4326'), ('EPSG', 4326)]
-        for pair in pairs:
-            expected = OrderedDict([pair])
-            assert utils.proj4.convert_proj_floats([pair]) == expected
-
-    def test_proj4_str_dict_conversion(self):
-        from pyresample import utils
-
-        proj_str = "+proj=lcc +ellps=WGS84 +lon_0=-95 +lat_1=25.5 +no_defs"
-        proj_dict = utils.proj4.proj4_str_to_dict(proj_str)
-        proj_str2 = utils.proj4.proj4_dict_to_str(proj_dict)
-        proj_dict2 = utils.proj4.proj4_str_to_dict(proj_str2)
-        assert proj_dict == proj_dict2
-        assert isinstance(proj_dict['lon_0'], (float, int))
-        assert isinstance(proj_dict2['lon_0'], (float, int))
-        assert isinstance(proj_dict['lat_1'], float)
-        assert isinstance(proj_dict2['lat_1'], float)
-
-        # EPSG
-        proj_str = '+init=EPSG:4326'
-        proj_dict = utils.proj4.proj4_str_to_dict(proj_str)
-        proj_str2 = utils.proj4.proj4_dict_to_str(proj_dict)
-        proj_dict2 = utils.proj4.proj4_str_to_dict(proj_str2)
-        # pyproj usually expands EPSG definitions so we can only round trip
-        assert proj_dict == proj_dict2
-
-        proj_str = 'EPSG:4326'
-        proj_dict_exp2 = {'proj': 'longlat', 'datum': 'WGS84', 'no_defs': None, 'type': 'crs'}
-        proj_dict = utils.proj4.proj4_str_to_dict(proj_str)
-        assert proj_dict == proj_dict_exp2
-        # input != output for this style of EPSG code
-        # EPSG to PROJ.4 can be lossy
-        # self.assertEqual(utils._proj4.proj4_dict_to_str(proj_dict), proj_str)  # round-trip
-
-    def test_proj4_str_dict_conversion_with_valueless_parameter(self):
-        from pyresample import utils
-
-        # Value-less south parameter
-        proj_str = "+ellps=WGS84 +no_defs +proj=utm +south +type=crs +units=m +zone=54"
-        proj_dict = utils.proj4.proj4_str_to_dict(proj_str)
-        proj_str2 = utils.proj4.proj4_dict_to_str(proj_dict)
-        proj_dict2 = utils.proj4.proj4_str_to_dict(proj_str2)
-        assert proj_dict == proj_dict2
-
-    @pytest.mark.skipif(pyproj.__proj_version__ == "9.3.0", reason="Bug in PROJ causes inequality in EPSG comparison")
-    def test_def2yaml_converter(self):
-        import tempfile
-
-        from pyresample import convert_def_to_yaml, parse_area_file
-        def_file = os.path.join(TEST_FILES_PATH, 'areas.cfg')
-        filehandle, yaml_file = tempfile.mkstemp()
-        os.close(filehandle)
-        try:
-            convert_def_to_yaml(def_file, yaml_file)
-            areas_new = set(parse_area_file(yaml_file))
-            areas = parse_area_file(def_file)
-            areas_old = set(areas)
-            areas_new = {area.area_id: area for area in areas_new}
-            areas_old = {area.area_id: area for area in areas_old}
-            assert areas_new == areas_old
-        finally:
-            os.remove(yaml_file)
+    from pyresample import convert_def_to_yaml, parse_area_file
+    def_file = os.path.join(TEST_FILES_PATH, 'areas.cfg')
+    filehandle, yaml_file = tempfile.mkstemp()
+    os.close(filehandle)
+    try:
+        convert_def_to_yaml(def_file, yaml_file)
+        areas_new = set(parse_area_file(yaml_file))
+        areas = parse_area_file(def_file)
+        areas_old = set(areas)
+        areas_new = {area.area_id: area for area in areas_new}
+        areas_old = {area.area_id: area for area in areas_old}
+        assert areas_new == areas_old
+    finally:
+        os.remove(yaml_file)
 
 
 def test_check_slice_orientation():
